21f66ea1b61cf82e982aed0c4212359b5b3b30a1
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-08-10 21:20:14 +0900

    Feat: 코인 리스트 리사이클러뷰 UI 및 연결 구현

--- app/src/main/java/com/nunu/coco/view/SelectActivity.kt
+++ app/src/main/java/com/nunu/coco/view/SelectActivity.kt
@@ -4,20 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nunu.coco.R
+import com.nunu.coco.databinding.ActivitySelectBinding
+import com.nunu.coco.view.adapter.SelectRvAdapter
 import timber.log.Timber
 
 class SelectActivity : AppCompatActivity() {
 
     private val viewModel : SelectViewModel by viewModels()
 
+    private lateinit var selectRvAdapter: SelectRvAdapter
+
+    private lateinit var binding : ActivitySelectBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select)
+        binding = ActivitySelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel.getCurrentCoinList()
         viewModel.currentPriceResult.observe(this, Observer {
+
+            selectRvAdapter = SelectRvAdapter(this, it)
+
+            binding.coinListRV.adapter = selectRvAdapter
+            binding.coinListRV.layoutManager = LinearLayoutManager(this)
+
             Timber.d(it.toString())
         })
 
