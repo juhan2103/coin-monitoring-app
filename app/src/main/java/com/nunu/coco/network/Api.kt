4548247d72da40a0873b1df2b9d6cd729811b76a
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-08-17 15:02:20 +0900

    Feat: 최근 변동 정보 API 불러오기 구현

--- app/src/main/java/com/nunu/coco/network/Api.kt
+++ app/src/main/java/com/nunu/coco/network/Api.kt
@@ -1,11 +1,16 @@
 package com.nunu.coco.network
 
 import com.nunu.coco.network.model.CurrentPriceList
+import com.nunu.coco.network.model.RecentCoinPriceList
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface Api {
 
     @GET("public/ticker/ALL_KRW")
     suspend fun getCurrentCoinList() : CurrentPriceList
+
+    @GET("public/transaction_history/{coin}_KRW")
+    suspend fun getRecentCoinPrice(@Path("coin") coin : String) : RecentCoinPriceList
 
 }
